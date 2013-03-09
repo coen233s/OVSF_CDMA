c9b0c9092f51445bfc4c69c32274d71728f51e81
coen233e <e>
2013-03-08 21:04:13 -0800

    Print length as int, and each code in hex.

--- dev/protocol/ProtocolData.cpp
+++ dev/protocol/ProtocolData.cpp
@@ -13,21 +13,22 @@ using namespace std;
 // dump struct ControlFrame
 ostream& operator<<(ostream& os, struct ControlFrame &cf)
 {
-	os << "c2s:" << (int)cf.c2s << " tr:" << (int)cf.tr
-	   << " ack:" << (int)cf.ack << " uid:" << (int)cf.uid
-	   << " size:" << (int)cf.data_size;
+    os << "c2s:" << (int)cf.c2s << " tr:" << (int)cf.tr
+        << " ack:" << (int)cf.ack << " uid:" << (int)cf.uid
+        << " size:" << (int)cf.data_size;
 
-	if (cf.c2s) {
-		RateRequest *pReq = reinterpret_cast<RateRequest *>(&cf.data);
-		os << " min_rate:" << (int)pReq->min_rate
-		   << " max_rate:" << (int)pReq->max_rate;
-	} else {
-		CodeAssignment *pCa = reinterpret_cast<CodeAssignment *>(&cf.data);
-        os << " code: ";
+    if (cf.c2s) {
+        RateRequest *pReq = reinterpret_cast<RateRequest *>(&cf.data);
+        os << " min_rate:" << (int)pReq->min_rate
+            << " max_rate:" << (int)pReq->max_rate;
+    } else {
+        CodeAssignment *pCa = reinterpret_cast<CodeAssignment *>(&cf.data);
+
+        os << " length:" << (int)pCa->length << " code:";
         for (uint32_t i = 0; i < pCa->length; ++i)
         {
-    		os << (int)pCa->code[i] << " ";
+            os << std::hex << (int)pCa->code[i] << " ";
         }
-	}
+    }
     return os;
 }
