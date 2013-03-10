60c149f93eff6e34663a9f7784cf0467d2dc5e46
Danke Xie <dankexie@hotmail.com>
2013-03-10 16:25:14 -0700

    Add missing files NamedObject.{cpp|h}

---
+++ NamedObject.cpp
@@ -0,0 +1,16 @@
+/*
+ * NamedObject.cpp
+ *
+ *  Created on: Mar 10, 2013
+ *      Author: Danke
+ */
+
+#include "NamedObject.h"
+
+NamedObject::NamedObject(const string &name)
+: m_name(name)
+{
+}
+
+NamedObject::~NamedObject() {
+}
