1447ff107946d65bc7f7e65b648fb9e4642028c5
coen233e <coen233e@gmail.com>
2013-03-17 11:53:36 -0700

    So that it works on Windoze

--- debug.h
+++ debug.h
@@ -2,13 +2,21 @@
 #define DEBUG_H_
 
 #ifdef DEBUG
+#ifdef WIN32
+#define dout(x) cout << x
+#else
 #define dout(x...) cout << x
+#endif
 #else
 #define dout(...)
 #endif
 
 #ifdef VERBOSE
+#ifdef WIN32
+#define vout(x) cout << x
+#else
 #define vout(x...) cout << x
+#endif
 #else
 #define vout(...)
 #endif
