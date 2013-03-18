2b0e27e554d54324407956e82491a6b28317e6c3
coen233e <coen233e@gmail.com>
2013-03-17 17:05:12 -0700

    Compare tick count

--- testall.sh
+++ testall.sh
@@ -1,17 +1,27 @@
+
 #make -j clean all
 
 pushd test/TestCdma
 rm *.out
 rm *.diff
 rm *.tick.txt
-for t in {1..6}
+for v in {1..2}
 do
-../../bin/TestCdma < test$t.txt
-for s in {2..5}
-do
-diff *Station.$s.in *Station.$s.out > $t.$s.diff
-done
+    if [ $v = 2 ]
+    then
+        opts='-f -o'
+    fi
+
+    for t in {1..6}
+    do
+        ../../bin/TestCdma $opts < test$t.txt > /dev/null
+        for s in {2..5}
+        do
+            mv MobileStation.$s.tick.txt MobileStation.$t.$s.tick.$v.txt
+            diff *Station.$s.in *Station.$s.out > $t.$s.diff
+        done
+    done
 done
 ls -l *.diff
+tail *.tick.*
 popd
-
