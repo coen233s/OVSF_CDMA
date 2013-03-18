bcb399f88998dd26e036558befeff0418184c655
coen233e <coen233e@gmail.com>
2013-03-17 18:51:56 -0700

    Added file existence check

--- testall.sh
+++ testall.sh
@@ -3,25 +3,29 @@
 
 pushd test/TestCdma
 rm *.out
-rm *.diff
-rm *.tick.txt
+rm *.diff*
+rm *.tick.*
+mode=variable_dynamic
 for v in {1..2}
 do
     if [ $v = 2 ]
     then
         opts='-f -o'
+        mode=fixed_once
     fi
 
     for t in {1..6}
     do
+        echo "../../bin/TestCdma $opts < test$t.txt"
         ../../bin/TestCdma $opts < test$t.txt > /dev/null
         for s in {2..5}
         do
-            mv MobileStation.$s.tick.txt MobileStation.$t.$s.tick.$v.txt
-            diff *Station.$s.in *Station.$s.out > $t.$s.diff
+            [ -f MobileStation.$s.tick.txt ] && mv MobileStation.$s.tick.txt MobileStation.$t.$s.tick.$mode.txt
+            [ -f *Station.$s.out ] && diff *Station.$s.in *Station.$s.out > $t.$s.$mode.diff
         done
     done
 done
 ls -l *.diff
 tail *.tick.*
+echo ""
 popd
